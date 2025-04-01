2f5c038c9bccbebf7114182f1147e7dd1ab4b188
Hayden Slyk <132722612+Slykdesign@users.noreply.github.com>
2025-04-01 14:34:14 -0400

    Update ext2.h

--- Step4/ext2.h
+++ Step4/ext2.h
@@ -16,19 +16,55 @@ typedef struct {
     uint32_t s_blocks_per_group;
     uint32_t s_frags_per_group;
     uint32_t s_inodes_per_group;
-    // ... other fields ...
+    uint32_t s_mtime;
+    uint32_t s_wtime;
+    uint16_t s_mnt_count;
+    uint16_t s_max_mnt_count;
     uint16_t s_magic;
+    uint16_t s_state;
+    uint16_t s_errors;
+    uint16_t s_minor_rev_level;
+    uint32_t s_lastcheck;
+    uint32_t s_checkinterval;
+    uint32_t s_creator_os;
+    uint32_t s_rev_level;
+    uint16_t s_def_resuid;
+    uint16_t s_def_resgid;
+    uint32_t s_first_ino;
+    uint16_t s_inode_size;
+    uint16_t s_block_group_nr;
+    uint32_t s_feature_compat;
+    uint32_t s_feature_incompat;
+    uint32_t s_feature_ro_compat;
+    uint8_t  s_uuid[16];
+    char     s_volume_name[16];
+    char     s_last_mounted[64];
+    uint32_t s_algorithm_usage_bitmap;
+    uint8_t  s_prealloc_blocks;
+    uint8_t  s_prealloc_dir_blocks;
+    uint16_t s_padding1;
+    uint8_t  s_journal_uuid[16];
+    uint32_t s_journal_inum;
+    uint32_t s_journal_dev;
+    uint32_t s_last_orphan;
+    uint32_t s_hash_seed[4];
+    uint8_t  s_def_hash_version;
+    uint8_t  s_reserved_char_pad;
+    uint16_t s_reserved_word_pad;
+    uint32_t s_default_mount_opts;
+    uint32_t s_first_meta_bg;
+    uint32_t s_reserved[190];
 } Ext2Superblock;
 
-// Define the Ext2 Block Group Descriptor structure
 typedef struct {
-    uint32_t bg_block_bitmap;      // Blocks bitmap block
-    uint32_t bg_inode_bitmap;      // Inodes bitmap block
-    uint32_t bg_inode_table;       // Inodes table block
-    uint16_t bg_free_blocks_count; // Free blocks count
-    uint16_t bg_free_inodes_count; // Free inodes count
-    uint16_t bg_used_dirs_count;   // Directories count
-    // ... other fields ...
+    uint32_t bg_block_bitmap;
+    uint32_t bg_inode_bitmap;
+    uint32_t bg_inode_table;
+    uint16_t bg_free_blocks_count;
+    uint16_t bg_free_inodes_count;
+    uint16_t bg_used_dirs_count;
+    uint16_t bg_pad;
+    uint32_t bg_reserved[3];
 } Ext2BlockGroupDescriptor;
 
 // Define the Ext2 Inode structure
