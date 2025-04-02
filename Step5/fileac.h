9e8caafed1b3e51501bcbb5a5ed408fe9d904801
Hayden Slyk <132722612+Slykdesign@users.noreply.github.com>
2025-04-02 10:46:11 -0400

    Update fileac.h

--- Step5/fileac.h
+++ Step5/fileac.h
@@ -3,6 +3,9 @@
 
 #include "ext2.h"
 
+typedef struct {
+} FileAccess;
+
 // Function prototypes for file access
 int32_t fetchBlockFromFile(Ext2Inode *inode, uint32_t bNum, void *buf);
 int32_t writeBlockToFile(Ext2Inode *inode, uint32_t bNum, void *buf);
